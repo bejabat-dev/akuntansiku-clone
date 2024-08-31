1613ecef675cd1bf070f24ca8b7b5a52e55aaca6
bejabat-dev <bejabattailor@gmail.com>
2024-08-31 14:08:25 +0700

    1.0

--- app/src/main/java/com/example/moliyafinance/laporan/JurnalUmum.kt
+++ app/src/main/java/com/example/moliyafinance/laporan/JurnalUmum.kt
@@ -1,21 +1,19 @@
 package com.example.moliyafinance.laporan
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.moliyafinance.R
+import com.example.moliyafinance.databinding.ActivityLaporanBinding
 
 class JurnalUmum : AppCompatActivity() {
+    private lateinit var bind : ActivityLaporanBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_jurnal_umum)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        bind = ActivityLaporanBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+    }
+
+    private fun init(){
+
     }
 }

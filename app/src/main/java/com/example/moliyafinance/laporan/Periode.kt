3896f9221612260fc1980f563661c81181b02e83
bejabat-dev <bejabattailor@gmail.com>
2024-09-29 22:22:00 +0700

    1.0

--- app/src/main/java/com/example/moliyafinance/laporan/Periode.kt
+++ app/src/main/java/com/example/moliyafinance/laporan/Periode.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.moliyafinance.databinding.ActivityPeriodeBinding
 import com.example.moliyafinance.databinding.DialogTanggalBinding
+import com.example.moliyafinance.models.Transaksi
+import com.example.moliyafinance.navigation.Dashboard
 
 class Periode : AppCompatActivity() {
     private lateinit var bind: ActivityPeriodeBinding
@@ -14,6 +16,8 @@ class Periode : AppCompatActivity() {
         init()
     }
 
+    data class Periode(val kategori:String,val innerAdapter:List<Transaksi>)
+
     private fun init() {
         bind.pilihTanggal.setOnClickListener {
             val dialogTanggalBinding = DialogTanggalBinding.inflate(layoutInflater)
@@ -21,5 +25,10 @@ class Periode : AppCompatActivity() {
         bind.back.setOnClickListener {
             finish()
         }
+
+        for(data in Dashboard.listTransaksi){
+
+        }
+
     }
 }

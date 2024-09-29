76f3834697c1b27cfb2fbf866512165829ff822a
bejabat-dev <bejabattailor@gmail.com>
2024-09-29 22:49:11 +0700

    1.0

---
+++ app/src/main/java/com/example/moliyafinance/adapters/AdapterPeriode.kt
@@ -0,0 +1,62 @@
+package com.example.moliyafinance.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moliyafinance.R
+import com.example.moliyafinance.laporan.Periode
+import com.example.moliyafinance.models.Transaksi
+
+class AdapterPeriode(
+    private val context: Context,
+    private val dataSet: List<Periode.Periode>
+) :
+    RecyclerView.Adapter<AdapterPeriode.ViewHolder>() {
+
+    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        val innerRecyclerView: RecyclerView = v.findViewById(R.id.recycler)
+        val kategori: TextView = v.findViewById(R.id.kategori)
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.recycler_periode, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(v: ViewHolder, pos: Int) {
+        val adapter = InnerAdapterPeriode(dataSet[pos].innerAdapter)
+        v.kategori.text = dataSet[pos].kategori
+        v.innerRecyclerView.adapter = adapter
+        v.innerRecyclerView.layoutManager = LinearLayoutManager(context)
+    }
+
+    override fun getItemCount() = dataSet.size
+}
+
+class InnerAdapterPeriode(
+    private val dataSet: List<Transaksi>
+) :
+    RecyclerView.Adapter<InnerAdapterPeriode.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tanggal: TextView = view.findViewById(R.id.tanggal)
+        val saldo: TextView = view.findViewById(R.id.saldo)
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.item_periode, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(view: ViewHolder, pos: Int) {
+
+    }
+
+    override fun getItemCount() = dataSet.size
+}
--- app/src/main/java/com/example/moliyafinance/laporan/Periode.kt
+++ app/src/main/java/com/example/moliyafinance/laporan/Periode.kt
@@ -2,6 +2,8 @@ package com.example.moliyafinance.laporan
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.moliyafinance.adapters.AdapterPeriode
 import com.example.moliyafinance.databinding.ActivityPeriodeBinding
 import com.example.moliyafinance.databinding.DialogTanggalBinding
 import com.example.moliyafinance.models.Transaksi
@@ -16,7 +18,7 @@ class Periode : AppCompatActivity() {
         init()
     }
 
-    data class Periode(val kategori:String,val innerAdapter:List<Transaksi>)
+    data class Periode(val kategori: String, val innerAdapter: List<Transaksi>)
 
     private fun init() {
         bind.pilihTanggal.setOnClickListener {
@@ -26,9 +28,27 @@ class Periode : AppCompatActivity() {
             finish()
         }
 
-        for(data in Dashboard.listTransaksi){
+        val kategori = ArrayList<String>()
+        val periodeList = ArrayList<Periode>()
 
+        for (data in Dashboard.listTransaksi) {
+            kategori.add(data.debit)
+            kategori.add(data.kredit)
         }
+        kategori.sort()
+        for (data in kategori) {
+            val innerList = ArrayList<Transaksi>()
+            for (newData in Dashboard.listTransaksi) {
+                if (newData.debit == data || newData.kredit == data) {
+                    innerList.add(newData)
+                }
+            }
+            val periode = Periode(data, innerList)
+            periodeList.add(periode)
+        }
+        val adapter = AdapterPeriode(this, periodeList)
+        bind.recycler.adapter = adapter
+        bind.recycler.layoutManager = LinearLayoutManager(this)
 
     }
 }

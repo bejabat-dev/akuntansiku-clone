76638d76822e634cf7cf89694b2c11935a416b94
bejabat-dev <bejabattailor@gmail.com>
2024-10-07 20:54:28 +0700

    1.0

--- app/src/main/java/com/example/moliyafinance/laporan/PerubahanModal.kt
+++ app/src/main/java/com/example/moliyafinance/laporan/PerubahanModal.kt
@@ -2,12 +2,8 @@ package com.example.moliyafinance.laporan
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.moliyafinance.adapters.AdapterPerubahanModal
 import com.example.moliyafinance.databinding.ActivityPerubahanModalBinding
 import com.example.moliyafinance.databinding.DialogTanggalBinding
-import com.example.moliyafinance.models.formatToRupiah
-import com.example.moliyafinance.navigation.Dashboard
 
 class PerubahanModal : AppCompatActivity() {
     private lateinit var bind: ActivityPerubahanModalBinding
@@ -16,37 +12,20 @@ class PerubahanModal : AppCompatActivity() {
         bind = ActivityPerubahanModalBinding.inflate(layoutInflater)
         setContentView(bind.root)
         init()
+        initClicks()
     }
 
-    data class PerubahanModal(val namaAkun: String, val debit: Int, val kredit: Int)
 
-    private fun init() {
+    private fun initClicks() {
         bind.pilihTanggal.setOnClickListener {
             val dialogTanggalBinding = DialogTanggalBinding.inflate(layoutInflater)
         }
         bind.back.setOnClickListener {
             finish()
         }
+    }
 
-        val listModal = ArrayList<PerubahanModal>()
+    private fun init(){
 
-        for (data in Dashboard.listTransaksi) {
-            if (data.kategoriDebit == "Modal") {
-                val modal = PerubahanModal(data.debit, data.nominal, 0)
-                listModal.add(modal)
-            } else if (data.kategoriKredit == "Modal") {
-                val modal = PerubahanModal(data.kredit, 0, data.nominal)
-                listModal.add(modal)
-            }
-        }
-
-        val adapterModal = AdapterPerubahanModal(listModal)
-        bind.recycler.adapter = adapterModal
-        bind.recycler.layoutManager = LinearLayoutManager(this)
-        bind.recycler.post {
-            val total = adapterModal.getTotal()
-            bind.totalModal.text = formatToRupiah(total)
-            bind.totalPerubahanModal.text = formatToRupiah(total)
-        }
     }
 }
---
+++ app/src/main/java/com/example/moliyafinance/models/PerubahanModal.kt
@@ -0,0 +1,4 @@
+package com.example.moliyafinance.models
+
+
+data class PerubahanModal(val namaAkun: String, val debit: Int, val kredit: Int)
